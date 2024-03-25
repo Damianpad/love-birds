529b5b0b0174fa54ace93aa19b4237608c885050
Damian Padilla <damipad4@gmail.com>
2024-03-25 16:33:08 -0500

    completed hero and GS base design

--- love-birds/src/Components/GetStarted.jsx
+++ love-birds/src/Components/GetStarted.jsx
@@ -1,13 +1,24 @@
-
 const GetStarted = () => {
-    return (
-        <>
-            <span>Get Started!</span>
-            <input type="text" />
-            <input type="text" />
-            <button>Sign up</button>
-        </>
-    );
-}
+  return (
+    <>
+      <span className="text-white text-xl m-2">Get Started!</span>
+      <div className="flex p-4 flex justify-around w-96">
+        <input
+          type="text"
+          placeholder="Email"
+          className="p-4 m-4 rounded-lg shadow-[2px_2px_9px] font-eb-garamond"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="p-4 m-4 rounded-lg shadow-[2px_2px_9px] font-eb-garamond"
+        />
+      </div>
+      <button className="ml-4 mr-4 pl-3 pr-3 pt-1 pb-1 border border-gray-500 rounded-md text-white bg-mainBlue font-eb-garamond w-40 ">
+        Sign up
+      </button>
+    </>
+  );
+};
 
-export default GetStarted
+export default GetStarted;
--- love-birds/src/Components/Hero.jsx
+++ love-birds/src/Components/Hero.jsx
@@ -2,8 +2,10 @@ import GetStarted from "./GetStarted";
 
 const Hero = () => {
   return (
-    <div className="bg-hero-pattern h-40 bg-cover bg-center">
-      <h1 className="text-white">Your Dream Wedding Starts Here</h1>
+    <div className="bg-hero-pattern bg-cover bg-center flex flex-col justify-center items-center h-96">
+      <h1 className="text-white font-eb-garamond text-5xl ">
+        Your Dream Wedding Starts Here
+      </h1>
       <GetStarted />
     </div>
   );

d414e712a731a6638b0baa52e7f136916a67d3c6
Damian Padilla <damipad4@gmail.com>
2024-12-06 14:54:17 -0600

    Extended Search Results to fit screen

--- love-birds/src/Components/SearchResults.jsx
+++ love-birds/src/Components/SearchResults.jsx
@@ -1,6 +1,6 @@
 const SearchResults = () => {
   return (
-    <div className="w-full flex justify-center bg-cardCream">
+    <div className="w-full min-h-screen flex justify-center bg-cardCream">
       <input
         type="text"
         className=" w-1/2 m-4 border-none rounded-xl h-10 "
